bf5656674c3738c6e616b0c549425acbec56b46e
Arun chandra roy <48654733+codersurjo@users.noreply.github.com>
2022-08-31 23:36:51 +0600

    Home page done

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+(function($){
+    jQuery(document).ready(function () {
+        jQuery('.toggle-label').click(function () {
+            jQuery('.offcanvas-container').toggleClass('active');
+
+        });
+    });
+    
+ }(jQuery));
