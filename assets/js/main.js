ea01877ed776cdc2c2301751bfc3b72f68708dea
Arun chandra roy <48654733+codersurjo@users.noreply.github.com>
2022-09-01 08:07:01 +0600

    All design

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,5 +5,58 @@
 
         });
     });
+
+    //owl carowsel
+    var owl = $(".single_group_team2");
+    owl.owlCarousel({
+        margin: 10,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout: 7000,
+         smartSpeed: 800,
+        nav: false,
+        dots:false,
+        responsive: {
+            0: {
+              items: 1
+            },
+
+            600: {
+              items: 3
+            },
+        
+            1024: {
+              items: 4
+            },
+        
+            1366: {
+              items: 5
+            }
+          }
+    });
+    //cocoma car
+    var owl = $(".all_cocoma");
+    owl.owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout: 7000,
+         smartSpeed: 800,
+        nav: false,
+        dots:false,
+        responsive: {
+            0: {
+              items: 1
+            },
+
+            600: {
+              items: 2
+            },
+        
+            1024: {
+              items: 3
+            }
+          }
+    });
     
  }(jQuery));
